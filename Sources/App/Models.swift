c198c15d632e44dde0a612f7bda63d4468998cb8
eddy10957 <eddytd96@gmail.com>
2023-05-18 16:55:29 +0200

    AirBadge

---
+++ Sources/App/Models.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Edoardo Troianiello on 18/05/23.
+//
+
+import Foundation
+
+
+//TODO:  Da cambiare- HAHAHAHA AIrby quanto ti divertirai a lavorare su questo Server
+struct Questions {
+    var daCambiare : String
+}
