b8df48526b6c8ad3580675d063d49db2f5ab6cf9
eddy10957 <eddytd96@gmail.com>
2023-05-19 10:50:39 +0200

    Added Models , Added Routes to be completed

--- Sources/App/Models.swift
+++ Sources/App/Models.swift
@@ -6,9 +6,86 @@
 //
 
 import Foundation
+import Vapor
 
+import Vapor
 
-//TODO:  Da cambiare- HAHAHAHA AIrby quanto ti divertirai a lavorare su questo Server
-struct Questions {
-    var daCambiare : String
+struct Party: Content {
+    var id: UUID?
+    var code: String
+    var isGameStarted: Bool
+    var teams: [Team]
+    var players: [Player]
+
+    init(code: String, isGameStarted: Bool = false, teams: [Team] = [], players: [Player] = []) {
+        self.id = UUID()
+        self.code = code
+        self.isGameStarted = isGameStarted
+        self.teams = teams
+        self.players = players
+    }
+}
+
+struct Player: Content {
+    var id: UUID?
+    var name: String
+    var score: Int
+    var teamID: UUID?
+
+    init(name: String, score: Int = 0, teamID: UUID? = nil) {
+        self.id = UUID()
+        self.name = name
+        self.score = score
+        self.teamID = teamID
+    }
+}
+
+struct Team: Content {
+    var id: UUID?
+    var name: String
+    var players: [Player]
+
+    init(name: String, players: [Player] = []) {
+        self.id = UUID()
+        self.name = name
+        self.players = players
+    }
+}
+
+struct Question: Content {
+    var id: UUID?
+    var text: String
+    var correctAnswer: String
+
+    init(text: String, correctAnswer: String) {
+        self.id = UUID()
+        self.text = text
+        self.correctAnswer = correctAnswer
+    }
+}
+
+struct Answer: Content {
+    var id: UUID?
+    var questionID: UUID
+    var playerID: UUID
+    var selectedAnswer: String
+
+    init(questionID: UUID, playerID: UUID, selectedAnswer: String) {
+        self.id = UUID()
+        self.questionID = questionID
+        self.playerID = playerID
+        self.selectedAnswer = selectedAnswer
+    }
+}
+
+struct LeaderboardEntry: Content {
+    var id: UUID?
+    var playerName: String
+    var score: Int
+
+    init(playerName: String, score: Int) {
+        self.id = UUID()
+        self.playerName = playerName
+        self.score = score
+    }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,7 +8,73 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
-    app.get("GeGiù") { req async -> String in
-        "Gesoo!"
+    
+    // Create a new party
+    app.post("parties") { req -> Party in
+        let party = try req.content.decode(Party.self)
+        // Save the party and return it
+        return party
     }
+
+    // Join an existing party
+    app.post("parties", ":partyID", "join") { req -> HTTPStatus in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        let player = try req.content.decode(Player.self)
+        // Join the party with the provided partyID
+        // Add the player to the party
+        return .ok
+    }
+
+    // Create a new team
+    app.post("teams") { req -> Team in
+        let team = try req.content.decode(Team.self)
+        // Save the team and return it
+        return team
+    }
+
+    // Add player to a team
+    app.post("teams", ":teamID", "players") { req -> HTTPStatus in
+        let teamID = try req.parameters.require("teamID", as: UUID.self)
+        let player = try req.content.decode(Player.self)
+        // Add the player to the team with the provided teamID
+        return .ok
+    }
+
+    // Start the game
+    app.post("parties", ":partyID", "start") { req -> HTTPStatus in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        // Start the game for the party with the provided partyID
+        return .ok
+    }
+
+    // Get party details
+    app.get("parties", ":partyID") { req -> Party in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        // Fetch the party details from the database using the partyID
+        // Return the party
+        return party
+    }
+
+    // Submit an answer
+    app.post("parties", ":partyID", "answers") { req -> HTTPStatus in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        let answer = try req.content.decode(Answer.self)
+        // Submit the answer for the party with the provided partyID
+        return .ok
+    }
+
+    // Get leaderboard (individual)
+    app.get("parties", ":partyID", "leaderboard", "individual") { req -> [LeaderboardEntry] in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        // Fetch and return the individual leaderboard for the party with the provided partyID
+        return leaderboardEntries
+    }
+
+    // Get leaderboard (team)
+    app.get("parties", ":partyID", "leaderboard", "team") { req -> [LeaderboardEntry] in
+        let partyID = try req.parameters.require("partyID", as: UUID.self)
+        // Fetch and return the team leaderboard for the party with the provided partyID
+        return leaderboardEntries
+    }
+
 }
